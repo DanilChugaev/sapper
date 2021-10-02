c00a7939ac3ac117146bdad26aadf476a64445e0
danilchugaev <dmchugaev@gmail.com>
2021-10-02 17:53:59 +0700

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-class Helper {
-
-    public component () : HTMLDivElement {
-        debugger
-        const element = document.createElement('div')
-        element.innerHTML = 'Hello, how are you? I am written in --- typescript.'
-        return element
-    }
-}
-
-document.body.appendChild(new Helper().component())
+// настройки
+    // тип
+    // дефолтные значения
+// инициализация поля
+// работа с канвас
+console.log('sd');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.ts',
   devtool: 'inline-source-map',
   module: {
@@ -20,4 +20,9 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
 };
